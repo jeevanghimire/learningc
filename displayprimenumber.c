c727bae7ee9e3a20dd2ddb239d205b9860b3ce22
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-06-02 12:45:38 +0545

    it's for ajust

---
+++ displayprimenumber.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of interger for data to calculate: "); //for knowing the lenght
+    scanf("%d", &n);
+    int number[n];
+    printf("Enter %d number: ", n);
+    int i;
+    for (i = 1; i < n; i++)
+    {
+        scanf("%d", &number[i]);
+        
+    }
+    for (i = 0; i < n; i++)
+    {
+        int num = number[i];
+        int j;
+        int is_prime = 1;
+
+        for (j = 2; j = n - 1; j++)
+        {
+            if (num % j == 0)
+            {
+                is_prime = 0;
+                break;
+            } 
+            {
+                printf("%d", num);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
