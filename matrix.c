c48e1a33fc53a0800396f15fa4032237ecac80ca
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-06-04 19:26:02 +0545

    -

---
+++ matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrixA[10], [10], m1, m2;
+    int matrixB[10], [10], b1, b2;
+    int i, j;
+    printf("Enter the number for martixA;");
+    scanf("%d%d", &m1, &m2);
+    printf("Enter the dimension for matrixB;");
+    scanf("%d%d", &b1, &b2);
+    for (i = 0; i < ; i++)
+    {
+        
+    }
+
+    return 0;
+}
