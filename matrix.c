597740036d0b557b9c2f3d1ac170efaad11ba854
Jeevan Ghimire <imjeevanghimire@gmail.com>
2021-06-05 12:50:15 +0545

    matix Addition

--- matrix.c
+++ matrix.c
@@ -2,16 +2,50 @@
 
 int main()
 {
-    int matrixA[10], [10], m1, m2;
-    int matrixB[10], [10], b1, b2;
+
+    float matA[2][2];
+    float matB[2][2];
+    float matC[2][2] = {0, 0, 0, 0};
+
+    printf("Enter the number for matrix A :\n ");
+
+    //soriing number in matrix A
     int i, j;
-    printf("Enter the number for martixA;");
-    scanf("%d%d", &m1, &m2);
-    printf("Enter the dimension for matrixB;");
-    scanf("%d%d", &b1, &b2);
-    for (i = 0; i < ; i++)
+    for (i = 0; i < 2; i++)
+
     {
-        
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%f", &matA[i][j]);
+        }
+    }
+    printf("Enter the number of matrixB:\n");
+    //soriing number for matrix matrix B
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%f", &matB[i][j]);
+        }
+    }
+
+    // Adding the both matrix
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            // now adding both matrixA and matrixB
+            matC[i][j] = matA[i][j] + matB[i][j];
+        }
+    }
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%f \t", matC[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
