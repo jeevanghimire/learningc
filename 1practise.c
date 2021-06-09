3bcdfa4cf29518b95b879cb6c492e117929faf55
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-06-09 08:22:21 -0700

    recover all

---
+++ 1practise.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int term = 1;
+    printf("Enter how many time you want the sum: ");
+    scanf(" %d", &a);
+    int sum = 0;
+    int i;
+    for (i = 1; i <= a; i++)
+    {
+        sum = sum + term;
+        term = term * 10 + 1;
+    }
+    printf("The sum is %d", sum);
+    return 0;
+}
---
+++ matrix.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float matA[2][2];
+    float matB[2][2];
+    float matC[2][2] = {0, 0, 0, 0};
+
+    printf("Enter the number for matrix A :\n ");
+
+    //soriing number in matrix A
+    int i, j;
+    for (i = 0; i < 2; i++)
+
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%f", &matA[i][j]);
+        }
+    }
+    printf("Enter the number of matrixB:\n");
+    //soriing number for matrix matrix B
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            scanf("%f", &matB[i][j]);
+        }
+    }
+
+    // Adding the both matrix
+
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            // now adding both matrixA and matrixB
+            matC[i][j] = matA[i][j] + matB[i][j];
+        }
+    }
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%f \t", matC[i][j]);
+        }
+        printf("\n");
+    }
+    //end
+    
+
+    return 0;
+}
