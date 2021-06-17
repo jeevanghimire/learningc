b6a00bee9b0ac03c841144b0d5d3edbe44928033
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-06-16 23:55:04 -0700

    challange check

--- Other Stuff/challange.c
+++ Other Stuff/challange.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int *maxinum_min(int *a, int *b)
 {
- (*a) > (*b) ?? return a,  return b;
+
+    (a > b) ? &a : &b;
 }
 
 int main()
 {
-int m = 55 , n = 100;
-int *p;
-p = maxinum_min(&m, &n);
-printf("maxium = %d\n", m);
-
+    int m = 55, n = 100;
+    int *p;
+    p = maxinum_min(&m, &n);
+    printf("maxinum = %d\n", *p);
 
     return 0;
 }
