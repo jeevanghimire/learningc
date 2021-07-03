ece5c155bc2d81a863824b7112a9bbb82d376087
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-07-03 12:16:08 +0545

    bit learning

---
+++ bit/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int bitwise(int n)
+{
+    int i;
+    for (i =n ; i < 0; i++)
+    {
+       
+        printf("%d", n);
+    }
+}
+int main()
+{
+    int n = 13;
+    printf("%d", bitwise(n));
+    return 0;
+}
