f65a994245cc1f677f0e1c48067f179ff5d74daa
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-07-06 16:18:14 +0545

    deleting unessary file

--- function/fabinicoseries.c
+++ function/fabinicoseries.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-  
 
-    void  print_fabonacco(int n){
-        //checking the number for fabbonacci series
-        printf("The fabiniko series are :");
-        int i,a=0,b=1;
-        for (i = 0;i<n;i++ ){
-            //programmin the fabiniko algo
-            printf("%d ",a);
-            int c = a +b;
-            a = b;
-            b = c;
-
-        }
+void print_fabonacco(int n)
+{
+    //checking the number for fabbonacci series
+    printf("The fabiniko series are :");
+    int i, a = 0, b = 1;
+    for (i = 0; i < n; i++)
+    {
+        //programming the fabiniko algo
+        printf("%ld ", a);
+        int c = a + b;
+        a = b;
+        b = c;
     }
-
+}
 
 int main()
 {
-int num;
-printf("Enter the number for fabbonacci series:");
-scanf("%d", &num);
-print_fabonacco(num);
+    int num;
+    printf("Enter the number for fabbonacci series:");
+    scanf("%d", &num);
+    print_fabonacco(num);
 
     return 0;
 }
