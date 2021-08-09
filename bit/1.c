5c7191cf2f2d61c14ca9b067aed0446dd1152689
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-08-09 20:21:50 +0100

    noting

--- bit/1.c
+++ bit/1.c
@@ -4,7 +4,7 @@ int main()
 {
     int a = 3;
     int b = 4;
-    printf("before swap\n");
+    printf("before swap\n");    
 
     printf("The value of a is %d\n", a);
     printf("The value of b is %d\n", b);
