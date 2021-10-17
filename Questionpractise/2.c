eef7e3b00ffe844f7b0516900ed5a96f182bcefd
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-10-17 07:52:39 +0545

    commiting file's

--- Questionpractise/2.c
+++ Questionpractise/2.c
@@ -1,28 +1,29 @@
 //Also failed
 
-
 #include <stdio.h>
 
 #include <string.h>
 #define newline printf("\n");
 int main()
 {
-int n[100];
-int result[100];
-printf("Enter the number for swap:");
-scanf("%s", n);
-int i, j,k;
-for (i = 0; i <n['\0']; i++){
-    for (j='\0';j<=i; j--){
-        int temp[100] = n[i];
-        n[i]=n[j];
-        n[j]=temp[i];
-        result[k] = temp[i];
-        printf("%d\n",result[k]);
-    }
+    int n[100];
+    int result[100];
+    printf("Enter the number for swap:");
+    scanf("%s", n);
+    int i, j, k;
+    for (i = 0; i < n['\0']; i++)
+    {
+        for (j = '\0'; j <= i; j--)
+        {
+            int temp[100] = n[i];
+            n[i] = n[j];
+            n[j] = temp[i];
+            result[k] = temp[i];
+            printf("%d\n", result[k]);
+        }
 
-newline
-}
+        newline
+    }
 
     return 0;
 }
