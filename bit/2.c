394cca085032dc0da1d401780b92859c90666c0a
Jeevan Ghimire <66736799+jeevanghimire@users.noreply.github.com>
2021-11-08 18:30:23 +0545

    checking only

--- bit/2.c
+++ bit/2.c
@@ -3,6 +3,17 @@
  int main() 
 {
     int n;
+    scanf("%d", &n);
+    if (n == 1)
+        printf("1");
+    else if (n == 2)
+
+        printf("11");
+    else if (n == 3)
+
+        printf("21");
+    else if (n == 4)
+    
     
      return 0;
 }
